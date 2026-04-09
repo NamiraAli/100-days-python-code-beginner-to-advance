e6f97f7310c53016ddf7f0426f9f52b19d52d105
Namira Ali <157734998+NamiraAli@users.noreply.github.com>
2026-04-09 23:23:38 +0530

    Create Question class for quiz functionality
    Define Question class with attributes for text and answer.

---
+++ Day-17/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    def __init__(self,text,answer):
+        self.text=text #quetext is the attribute which will take the value from text from method call
+        self.ans=answer
+
+# new_q=Question("wertyui","rtyui")
