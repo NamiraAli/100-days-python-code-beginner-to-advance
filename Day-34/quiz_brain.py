a27f18c69e53c2a1633d19446b9258063fd996ae
Namira Ali <157734998+NamiraAli@users.noreply.github.com>
2026-05-05 13:31:28 +0530

    Add QuizBrain class for quiz functionality
    Implement QuizBrain class to manage quiz questions and scoring.

---
+++ Day-34/quiz_brain.py
@@ -0,0 +1,34 @@
+import html
+# html is used to unescape the text as before it was using sybols like &quote etc instead of " mark
+
+
+
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        q_text= html.unescape(self.current_question.text)
+        question_text=f"Q.{self.question_number}: {q_text}" #this gets question from api
+        return question_text
+
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer)
+
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
+        else:
+            return False
+
